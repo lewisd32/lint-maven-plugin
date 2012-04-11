6118c82b64151eccf499ad18217e6f0c86325fa1
lewisd <derek@lewisd.com>
2012-04-11 12:48:00 -0700

    Two passing integration tests, one of which executes a rule, which raises a violation. Using spring to configure rules.

--- src/main/java/com/lewisd/maven/lint/ResultCollector.java
+++ src/main/java/com/lewisd/maven/lint/ResultCollector.java
@@ -1,5 +1,13 @@
 package com.lewisd.maven.lint;
 
+import org.apache.maven.project.MavenProject;
+
 public interface ResultCollector {
+
+	void writeSummary();
+
+	void addViolation(MavenProject mavenProject, String message, Object object);
+
+	boolean hasViolations();
 
 }
