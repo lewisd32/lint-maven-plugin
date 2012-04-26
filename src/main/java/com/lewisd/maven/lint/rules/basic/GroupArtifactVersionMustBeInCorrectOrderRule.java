e08b60ab692006ce91fa07878f180a43c1d04cb9
Derek Lewis <derek@lewisd.com>
2012-04-26 16:24:24 -0700

    Working on improving GAVOrder rule.

--- src/main/java/com/lewisd/maven/lint/rules/basic/GroupArtifactVersionMustBeInCorrectOrderRule.java
+++ src/main/java/com/lewisd/maven/lint/rules/basic/GroupArtifactVersionMustBeInCorrectOrderRule.java
@@ -2,6 +2,7 @@ package com.lewisd.maven.lint.rules.basic;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,9 +11,15 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import javax.sound.sampled.Port;
+
+import org.apache.maven.model.Dependency;
 import org.apache.maven.model.InputLocation;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.project.MavenProject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 
 import com.lewisd.maven.lint.ResultCollector;
 import com.lewisd.maven.lint.rules.AbstractRule;
@@ -21,22 +28,23 @@ import com.lewisd.maven.lint.util.ModelUtil;
 
 public class GroupArtifactVersionMustBeInCorrectOrderRule extends AbstractRule {
 	
-	private static final List<Object> sortOrder = new LinkedList<Object>();
+//	private static final List<Object> sortOrder = new LinkedList<Object>();
+	private final Map<Class, List<String>> expectedOrderByClass = new HashMap<Class, List<String>>();
 	
-	static {
-		sortOrder.add("");
-		sortOrder.add("modelVersion"); // for <project>
-		sortOrder.add("extensions"); // for <plugin>
-		sortOrder.add("groupId");
-		sortOrder.add("artifactId");
-		sortOrder.add("classifier");
-		sortOrder.add("type"); // for <dependencies>
-		sortOrder.add("version");
-		sortOrder.add("scope"); // for <dependencies>
-		
-		// We don't enforce the order of <configuration>, <exclusions>, <executions>, <dependencies>, and any other
-		// unlisted elements, other than they must be after the ones we do enforce.
-	}
+//	static {
+//		sortOrder.add("");
+//		sortOrder.add("modelVersion"); // for <project>
+//		sortOrder.add("extensions"); // for <plugin>
+//		sortOrder.add("groupId");
+//		sortOrder.add("artifactId");
+//		sortOrder.add("classifier");
+//		sortOrder.add("type"); // for <dependencies>
+//		sortOrder.add("version");
+//		sortOrder.add("scope"); // for <dependencies>
+//		
+//		// We don't enforce the order of <configuration>, <exclusions>, <executions>, <dependencies>, and any other
+//		// unlisted elements, other than they must be after the ones we do enforce.
+//	}
 	
 	@Autowired
 	public GroupArtifactVersionMustBeInCorrectOrderRule(final ExpressionEvaluator expressionEvaluator, final ModelUtil modelUtil) {
@@ -51,12 +59,28 @@ public class GroupArtifactVersionMustBeInCorrectOrderRule extends AbstractRule {
 	public String getIdentifier() {
 		return "GAVOrder";
 	}
+	
+	@Required
+	public void setOrderForProject(List<String> expectedOrder) {
+		expectedOrderByClass.put(Model.class, expectedOrder);
+	}
 
+	@Required
+	public void setOrderForDependency(List<String> expectedOrder) {
+		expectedOrderByClass.put(Dependency.class, expectedOrder);
+	}
+	
+	@Required
+	public void setOrderForPlugin(List<String> expectedOrder) {
+		expectedOrderByClass.put(Plugin.class, expectedOrder);
+	}
+	
 	public void invoke(final MavenProject mavenProject, final Map<String, Object> models, final ResultCollector resultCollector) {
 		
 		final Collection<Object> objectsToCheck = modelUtil.findGAVObjects(mavenProject);
 		
 		for (final Object object: objectsToCheck) {
+			final List<String> sortOrder = findSortOrder(object.getClass()); 
 			final Map<Object, InputLocation> locations = modelUtil.getLocations(object);
 
 			// Sort the locations by their location in the file
@@ -68,7 +92,7 @@ public class GroupArtifactVersionMustBeInCorrectOrderRule extends AbstractRule {
 
 			final List<Object> expectedOrderElements = new LinkedList<Object>();
 			// First, find the elements that do exist, for which we care about the order
-			for (final Object location : sortOrder) {
+			for (final String location : sortOrder) {
 				if (locations.containsKey(location)) {
 					expectedOrderElements.add(location);
 				}
@@ -101,6 +125,16 @@ public class GroupArtifactVersionMustBeInCorrectOrderRule extends AbstractRule {
 		}
 	}
 	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private List<String> findSortOrder(Class<? extends Object> klass) {
+		for (Class potentialClass : expectedOrderByClass.keySet()) {
+			if (potentialClass.isAssignableFrom(klass)) {
+				return expectedOrderByClass.get(potentialClass);
+			}
+		}
+		throw new IllegalStateException("No expected sort order found for class " + klass);
+	}
+
 	private class InputLocationMapValueComparator implements Comparator<InputLocation> {
 
 		@Override
