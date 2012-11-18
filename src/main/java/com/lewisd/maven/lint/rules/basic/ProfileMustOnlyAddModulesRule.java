93e044512a03c2b7989945d2aa71e5fd014e9409
Derek Lewis <derek@lewisd.com>
2012-11-18 11:20:05 -0800

    Converting expression evaluator to use JXPath. Adding rule for with-* profiles.

---
+++ src/main/java/com/lewisd/maven/lint/rules/basic/ProfileMustOnlyAddModulesRule.java
@@ -0,0 +1,82 @@
+package com.lewisd.maven.lint.rules.basic;
+
+import java.beans.PropertyDescriptor;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.apache.commons.jxpath.JXPathBeanInfo;
+import org.apache.commons.jxpath.JXPathIntrospector;
+import org.apache.commons.jxpath.util.ValueUtils;
+import org.apache.maven.model.InputLocation;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Profile;
+import org.apache.maven.project.MavenProject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.common.collect.Sets;
+import com.lewisd.maven.lint.ResultCollector;
+import com.lewisd.maven.lint.rules.AbstractRule;
+import com.lewisd.maven.lint.util.ExpressionEvaluator;
+import com.lewisd.maven.lint.util.ModelUtil;
+
+public class ProfileMustOnlyAddModulesRule extends AbstractRule {
+
+	private String patternString = "with-.*";
+	private Pattern pattern = Pattern.compile(patternString);
+	private final Set<String> allowableDescriptors = Sets.newHashSet("id", "activation", "modules", "source");
+
+	@Autowired
+	public ProfileMustOnlyAddModulesRule(final ExpressionEvaluator expressionEvaluator, final ModelUtil modelUtil) {
+		super(expressionEvaluator, modelUtil);
+	}
+
+	@Override
+	protected void addRequiredModels(final Set<String> requiredModels) {
+	}
+
+	public void setPattern(final String patternString) {
+		this.patternString = patternString;
+		pattern = Pattern.compile(patternString);
+	}
+
+	@Override
+	public String getIdentifier() {
+		return "ProfileOnlyAddModules";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Profiles who's ids match the pattern " + patternString + " must only add modules to the reactor.";
+	}
+
+	@Override
+    public void invoke(final MavenProject mavenProject, final Map<String, Object> models, final ResultCollector resultCollector) {
+		final Model originalModel = mavenProject.getOriginalModel();
+		final Collection<Profile> profiles = expressionEvaluator.getPath(originalModel, "/profiles");
+		System.err.println("PROFILES: " + profiles);
+		for (final Profile profile : profiles) {
+			if (profile.getId() != null && pattern.matcher(profile.getId()).matches()) {
+				final JXPathBeanInfo profileBeanInfo = JXPathIntrospector.getBeanInfo(Profile.class);
+				final PropertyDescriptor[] propertyDescriptors = profileBeanInfo.getPropertyDescriptors();
+                final List<String> disallowedDescriptors = new LinkedList<String>();
+				for (final PropertyDescriptor descriptor : propertyDescriptors) {
+					if (!allowableDescriptors.contains(descriptor.getName())) {
+						final Object value = ValueUtils.getValue(profile, descriptor);
+						if (value != null) {
+                            disallowedDescriptors.add(descriptor.getName());
+						}
+					}
+				}
+                if (!disallowedDescriptors.isEmpty()) {
+                    final InputLocation location = modelUtil.getLocation(profile, "");
+                    resultCollector.addViolation(mavenProject, this, "Found '" + disallowedDescriptors + "' where only submodules are allowed", location);
+                }
+			}
+		}
+	}
+
+}
--- src/main/java/com/lewisd/maven/lint/util/ExpressionEvaluator.java
+++ src/main/java/com/lewisd/maven/lint/util/ExpressionEvaluator.java
@@ -1,56 +1,34 @@
 package com.lewisd.maven.lint.util;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.jxpath.JXPathContext;
 
 public class ExpressionEvaluator {
 	
-	private final ReflectionUtil reflectionUtil;
-
-	@Autowired
-	public ExpressionEvaluator(ReflectionUtil reflectionUtil) {
-		this.reflectionUtil = reflectionUtil;
-	}
-
+	@SuppressWarnings("unchecked")
 	public <T> Collection<T> getPath(Object root, String path) {
-		final Collection<T> objects = new LinkedList<T>();
-		
-		getPath(root, path, objects);
-		
-		return objects;
-	}
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private <T> void getPath(Object root, String path, Collection<T> objects) {
-		final String[] pathParts = path.split("/", 2);
-		
-		final String element = pathParts[0];
-		
-		final char firstCharacter = Character.toUpperCase(element.charAt(0));
-		
-		String methodName = "get" + Character.toString(firstCharacter) + element.substring(1);
-		
-		T returned = (T) reflectionUtil.callMethod(root, methodName, new Class[] {}, new Object[] {});
-		if (returned != null) {
-			if (returned instanceof Collection) {
-				if (pathParts.length > 1) {
-					for(Object object : (Collection)returned) {
-						getPath(object, pathParts[1], objects);
-					}
-				} else {
-					objects.addAll((Collection)returned);
-				}
-			} else {
-				if (pathParts.length > 1) {
-					getPath(returned, pathParts[1], objects);
-				} else {
-					objects.add(returned);
-				}
-			}
-		}
-	}
-	
+		final List<T> list = new LinkedList<T>();
+		final JXPathContext context = JXPathContext.newContext(root);
 
+		for (final Iterator<?> i = context.iterate(path); i.hasNext();) {
+			final T next = (T) i.next();
+			list.add(next);
+		}
+		return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> T getFirst(Object root, String path) {
+		final JXPathContext context = JXPathContext.newContext(root);
+
+		for (final Iterator<?> i = context.iterate(path); i.hasNext();) {
+			return (T) i.next();
+		}
+		return null;
+	}
+	
 }
