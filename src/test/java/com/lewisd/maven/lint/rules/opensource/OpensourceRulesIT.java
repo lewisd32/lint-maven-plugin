91f5bbb517bd837b496bbf1771ee2feecf602d28
lkwg82 <lkwg82@gmx.de>
2013-08-25 15:09:21 +0200

    added MissingDeveloperInformationRule

--- src/test/java/com/lewisd/maven/lint/rules/opensource/OpensourceRulesIT.java
+++ src/test/java/com/lewisd/maven/lint/rules/opensource/OpensourceRulesIT.java
@@ -50,7 +50,7 @@ public class OpensourceRulesIT {
     @POM("src/test/resources/it-opensource-violations.xml")
     public void test() throws IOException, XmlPullParserException {
 
-        List<? extends AbstractRule> rules = Lists.newArrayList(new MissingCIManagementInformationRule(),new MissingIssueManagementInformationRule(), new MissingUrlRule(),new MissingInceptionYearRule(),new MissingLicenseRule());
+        List<? extends AbstractRule> rules = Lists.newArrayList(new MissingCIManagementInformationRule(), new MissingIssueManagementInformationRule(), new MissingUrlRule(), new MissingInceptionYearRule(), new MissingLicenseRule(), new MissingDeveloperInformationRule());
 
         for (AbstractRule rule : rules) {
             invokeRule(rule);
