2734c85927152705bd6bc7b2e09fe45cffac5f64
lewisd32 <github@lewisd.com>
2015-01-25 15:00:49 -0800

    Fixing import of deprecated package.

--- src/test/java/com/lewisd/maven/lint/ProjectModelsTest.java
+++ src/test/java/com/lewisd/maven/lint/ProjectModelsTest.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.apache.maven.project.MavenProject;
 import org.hamcrest.BaseMatcher;
